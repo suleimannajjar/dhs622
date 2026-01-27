735787a1aa2688bf16d7464694afa092ec75540b
suleimannajjar <soci105salmon@gmail.com>
2026-01-27 15:48:00 +0300

    Initial commit for week5 folder.

---
+++ week5/week5/utilities/db.py
@@ -0,0 +1,321 @@
+import sqlalchemy as sa
+from sqlalchemy.sql.schema import Table as SQLAlchemyTable
+from datetime import datetime
+from ..config import config
+
+
+def instantiate_channel_metadata_table(my_table_name: str) -> SQLAlchemyTable:
+    my_table = sa.Table(
+        my_table_name,
+        meta,
+        sa.Column("channel_id", sa.types.BIGINT, primary_key=True),
+        sa.Column("channel_name", sa.types.TEXT, unique=True),
+        sa.Column("channel_title", sa.types.TEXT, default=None),
+        sa.Column("channel_birthdate", sa.types.DateTime(timezone=True)),
+        sa.Column("channel_bio", sa.types.TEXT, default=None),
+        sa.Column("num_subscribers", sa.types.INTEGER, default=None),
+        sa.Column("data_source", sa.types.TEXT, default="telegram-api"),
+        sa.Column(
+            "checkup_time", sa.types.DateTime(timezone=True), default=datetime.utcnow
+        ),
+        sa.Column("api_response", sa.types.JSON, nullable=False),
+    )
+    return my_table
+
+
+def instantiate_seed_table(my_table_name: str) -> SQLAlchemyTable:
+    my_table = sa.Table(
+        my_table_name,
+        meta,
+        sa.Column("channel_id", sa.types.BIGINT, primary_key=True),
+        sa.Column("channel_name", sa.types.TEXT, nullable=False),
+        sa.Column("seed_list", sa.types.TEXT, primary_key=True),
+    )
+    return my_table
+
+
+def insert_data_into_channel_metadata_table(records: list[dict]):
+    stmt = sa.insert(channel_metadata_table).values(records)
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
+
+
+def insert_data_into_channel_metadata_table_advanced(records: list[dict]):
+    # check if the table already contains channels matching the IDs of the incoming records
+    stmt = sa.select(channel_metadata_table.c.channel_id).where(
+        channel_metadata_table.c.channel_id.in_(
+            [record["channel_id"] for record in records]
+        )
+    )
+    with engine.connect() as conn:
+        rp = conn.execute(stmt)
+        rows = rp.fetchall()
+    if len(rows) > 0:
+        rows = [x for (x,) in rows]
+
+    new_records = [record for record in records if record["channel_id"] not in rows]
+    duplicate_records = [record for record in records if record["channel_id"] in rows]
+
+    if len(new_records) > 0:
+        # save new_records
+        try:
+            stmt = sa.insert(channel_metadata_table).values(new_records)
+            with engine.connect() as conn:
+                conn.execute(stmt)
+                conn.commit()
+        except Exception as e:
+            print(e)
+    if len(duplicate_records) > 0:
+        pass  # do nothing with these
+
+
+def insert_data_into_seed_table(records: list[dict]):
+    stmt = sa.insert(seed_table).values(records)
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
+
+
+# TODO: consider creating an insert_data_into_seed_table_advanced() function
+
+
+def instantiate_channel_messages_table(my_table_name: str) -> SQLAlchemyTable:
+    my_table = sa.Table(
+        my_table_name,
+        meta,
+        sa.Column("channel_id", sa.types.BIGINT, primary_key=True),
+        sa.Column("message_id", sa.types.INTEGER, primary_key=True),
+        sa.Column("message_datetime", sa.types.DateTime(timezone=True)),
+        sa.Column("message_views", sa.types.INTEGER, default=None),
+        sa.Column("message_forwards", sa.types.INTEGER, default=None),
+        sa.Column("message_text", sa.types.TEXT, default=None),
+        sa.Column("forwardee_channel_id", sa.types.BIGINT, default=None),
+        sa.Column("forwardee_message_id", sa.types.INTEGER, default=None),
+        sa.Column("message_is_forward", sa.types.BOOLEAN, default=None),
+        sa.Column("data_source", sa.types.TEXT, default="telegram-api"),
+        sa.Column(
+            "checkup_time", sa.types.DateTime(timezone=True), default=datetime.utcnow
+        ),
+        sa.Column("api_response", sa.types.JSON, nullable=False),
+    )
+    return my_table
+
+
+def insert_data_into_channel_messages_table(records: list[dict]):
+    stmt = sa.insert(channel_message_table).values(records)
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
+
+
+def insert_data_into_channel_messages_table_advanced(records: list[dict]):
+    """
+    This function receives a list of records to insert into the channel messages SQL table.
+    That table, however, imposes a primary key restriction on unique tuples (channel_id, message_id).
+    Incoming messages should therefore be partitioned into new_records and duplicate_records, where
+    new records have (channel_id, message_id) tuples that do not match any records in the table, while
+    # duplicate records do already have matches in the table. These two partitions should be handled
+    differently.
+    """
+
+    stmt = sa.select(
+        channel_message_table.c.channel_id, channel_message_table.c.message_id
+    ).filter(
+        channel_message_table.c.channel_id.in_(
+            [record["channel_id"] for record in records]
+        ),
+        channel_message_table.c.message_id.in_(
+            [record["message_id"] for record in records]
+        ),
+    )
+
+    with engine.connect() as conn:
+        rp = conn.execute(stmt)
+        rows = rp.fetchall()
+    if len(rows) > 0:
+        # there is some overlap, so there would be an integrity violation if you attempted to write
+        # these records to the database. Instead, let's partition the records to those that are ok
+        # to write to the table, and those that are not ok
+        new_records = [
+            record
+            for record in records
+            if (record["channel_id"], record["message_id"]) not in rows
+        ]
+        duplicate_records = [record for record in records if record not in new_records]
+    else:
+        new_records = records
+        duplicate_records = []
+
+    if len(new_records) > 0:
+        stmt = sa.insert(channel_message_table).values(new_records)
+        with engine.connect() as conn:
+            conn.execute(stmt)
+            conn.commit()
+    if len(duplicate_records) > 0:
+        pass  # Exercise: add code here to update rows, replacing old data with new
+
+    return
+
+
+def fetch_seed_list_names() -> list[dict]:
+    with engine.connect() as conn:
+        rp = conn.execute(
+            sa.select(seed_table.c.seed_list).distinct(seed_table.c.seed_list)
+        )
+        rows = rp.fetchall()
+    return [row for (row,) in rows]
+
+
+def fetch_seed_list_preview(my_seed_list_names: list[str]) -> list[dict]:
+    with engine.connect() as conn:
+        rp = conn.execute(
+            sa.select(seed_table).where(seed_table.c.seed_list.in_(my_seed_list_names))
+        )
+    records = [dict(elt._mapping) for elt in rp.fetchall()]
+    return records
+
+
+def fetch_birth_chart_data(
+    seed_list_names: list[str], birth_chart_unit: str
+) -> list[dict]:
+    seed_channel_ids = list(
+        set([seed["channel_id"] for seed in fetch_seed_list_preview(seed_list_names)])
+    )
+
+    stmt = (
+        sa.select(
+            sa.sql.func.date_trunc(
+                birth_chart_unit, channel_metadata_table.c.channel_birthdate
+            ),
+            sa.sql.func.count(),
+        )
+        .filter(channel_metadata_table.c.channel_id.in_(seed_channel_ids))
+        .group_by(
+            sa.sql.func.date_trunc(
+                birth_chart_unit, channel_metadata_table.c.channel_birthdate
+            )
+        )
+        .order_by(
+            sa.sql.func.date_trunc(
+                birth_chart_unit, channel_metadata_table.c.channel_birthdate
+            )
+        )
+    )
+    with engine.connect() as conn:
+        rp = conn.execute(stmt)
+    records = [dict(elt._mapping) for elt in rp.fetchall()]
+    return [
+        {"creation_dt": record["date_trunc_1"], "count": record["count_1"]}
+        for record in records
+    ]
+
+
+def fetch_time_series_chart_data(
+    seed_list_names: list[str],
+    start_date: str,
+    end_date: str,
+    time_series_chart_unit: str,
+) -> list[dict]:
+    seed_channel_ids = list(
+        set([seed["channel_id"] for seed in fetch_seed_list_preview(seed_list_names)])
+    )
+
+    stmt = (
+        sa.select(
+            sa.sql.func.date_trunc(
+                time_series_chart_unit, channel_message_table.c.message_datetime
+            ),
+            sa.sql.func.count(),
+        )
+        .filter(
+            channel_message_table.c.channel_id.in_(seed_channel_ids),
+            channel_message_table.c.message_datetime
+            >= datetime.strptime(start_date, "%Y-%m-%d"),
+            channel_message_table.c.message_datetime
+            <= datetime.strptime(end_date, "%Y-%m-%d"),
+        )
+        .group_by(
+            sa.sql.func.date_trunc(
+                time_series_chart_unit, channel_message_table.c.message_datetime
+            )
+        )
+        .order_by(
+            sa.sql.func.date_trunc(
+                time_series_chart_unit, channel_message_table.c.message_datetime
+            )
+        )
+    )
+
+    with engine.connect() as conn:
+        rp = conn.execute(stmt)
+    records = [dict(elt._mapping) for elt in rp.fetchall()]
+    return [
+        {"message_dt": record["date_trunc_1"], "count": record["count_1"]}
+        for record in records
+    ]
+
+
+def fetch_top_messages(
+    seed_list_names: list[str], start_date: str, end_date: str, the_limit: int
+) -> list[dict]:
+    seed_channel_ids = list(
+        set([seed["channel_id"] for seed in fetch_seed_list_preview(seed_list_names)])
+    )
+
+    with engine.connect() as conn:
+        rp = conn.execute(
+            sa.select(channel_message_table)
+            .filter(
+                channel_message_table.c.channel_id.in_(seed_channel_ids),
+                channel_message_table.c.message_views.is_not(None),
+                channel_message_table.c.message_datetime
+                >= datetime.strptime(start_date, "%Y-%m-%d"),
+                channel_message_table.c.message_datetime
+                <= datetime.strptime(end_date, "%Y-%m-%d"),
+            )
+            .order_by(channel_message_table.c.message_views.desc())
+            .limit(the_limit)
+        )
+    records = [dict(elt._mapping) for elt in rp.fetchall()]
+    return records
+
+
+def fetch_seed_metadata_full(seed_list_names: list[str]) -> list[dict]:
+    seed_channel_ids = list(
+        set([seed["channel_id"] for seed in fetch_seed_list_preview(seed_list_names)])
+    )
+
+    with engine.connect() as conn:
+        rp = conn.execute(
+            sa.select(channel_metadata_table)
+            .where(channel_metadata_table.c.channel_id.in_(seed_channel_ids))
+            .order_by(channel_metadata_table.c.num_subscribers.desc())
+        )
+    records = [dict(elt._mapping) for elt in rp.fetchall()]
+    return records
+
+
+channel_message_table_name = "channel_messages"
+channel_metadata_table_name = "channel_metadata"
+seed_table_name = "seeds"
+
+engine = sa.create_engine(
+    f"postgresql://"
+    f"{config['telegram-db']['user']}:"
+    f"{config['telegram-db']['password']}"
+    f"@{config['telegram-db']['host']}:"
+    f"{config['telegram-db']['port']}/"
+    f"{config['telegram-db']['dbname']}",
+    echo=True,
+    pool_size=20,
+    max_overflow=0,
+)
+
+
+# Define Telegram tables and create them if they don't already exist:
+meta = sa.MetaData()
+channel_message_table = instantiate_channel_messages_table(channel_message_table_name)
+channel_metadata_table = instantiate_channel_metadata_table(channel_metadata_table_name)
+seed_table = instantiate_seed_table(seed_table_name)
+meta.create_all(engine)
---
+++ week5/week5/utilities/logic.py
@@ -0,0 +1,317 @@
+import time
+from telethon.errors.rpcerrorlist import UsernameInvalidError
+from telethon.sync import TelegramClient
+from telethon import functions
+from telethon.tl.patched import Message as TelegramMessage
+from telethon.tl.types.messages import ChatFull
+
+import pandas as pd
+from .db import (
+    fetch_seed_list_names,
+    fetch_seed_list_preview,
+    fetch_seed_metadata_full,
+    fetch_birth_chart_data,
+    fetch_time_series_chart_data,
+    fetch_top_messages,
+    insert_data_into_seed_table,
+    insert_data_into_channel_metadata_table_advanced,
+    insert_data_into_channel_messages_table_advanced,
+)
+
+SECONDS_TO_PAUSE_BETWEEN_CHANNEL_INFO_LOOKUPS = 30
+
+
+def extract_data_dictionary_from_channel_object(
+    channel_object: ChatFull, channel_name: str
+) -> dict:
+    return {
+        "channel_name": channel_name,
+        "channel_id": channel_object.to_dict()["full_chat"]["id"],
+        "channel_title": channel_object.to_dict()["chats"][0]["title"],
+        "num_subscribers": channel_object.to_dict()["full_chat"]["participants_count"],
+        "channel_bio": channel_object.to_dict()["full_chat"]["about"],
+        "channel_birthdate": channel_object.to_dict()["chats"][0]["date"],
+        "api_response": channel_object.to_json(),
+    }
+
+
+def retrieve_channel_metadata(
+    channel_names: list[str], app_name: str, api_id: int, api_hash: str
+) -> list[dict]:
+    # Retrieve data from Telegram API, using Telethon:
+    records = []
+    with TelegramClient(app_name, api_id, api_hash) as client:
+        for channel_name in channel_names:
+            print(f"Querying Telegram API for @{channel_name}...")
+            try:
+                channel_object = client(
+                    functions.channels.GetFullChannelRequest(channel=channel_name)
+                )
+            except ValueError as e:
+                print(e)
+                channel_object = None
+            except UsernameInvalidError as e:
+                print(e)
+                channel_object = None
+            except Exception as e:
+                raise e
+
+            if channel_object is not None:
+                records.append(
+                    extract_data_dictionary_from_channel_object(
+                        channel_object, channel_name
+                    )
+                )
+            else:
+                print(f"No metadata returned by Telegram API for @{channel_name}")
+
+            print(
+                f"Pausing {SECONDS_TO_PAUSE_BETWEEN_CHANNEL_INFO_LOOKUPS} seconds "
+                f"to respect Telegram API rate limits..."
+            )
+            time.sleep(
+                SECONDS_TO_PAUSE_BETWEEN_CHANNEL_INFO_LOOKUPS
+            )  # pause to respect API rate limiting
+
+    return records
+
+
+def retrieve_and_save_channel_metadata(
+    channel_names: list[str],
+    app_name: str,
+    api_id: int,
+    api_hash: str,
+    seed_list_name: str,
+):
+    # Retrieve data from Telegram API
+    records = retrieve_channel_metadata(channel_names, app_name, api_id, api_hash)
+
+    # Prepare seed data
+    seed_records = [
+        {
+            "channel_name": record["channel_name"],
+            "channel_id": record["channel_id"],
+            "seed_list": seed_list_name,
+        }
+        for record in records
+    ]
+
+    # Insert data into database
+    if len(records) > 0:
+        insert_data_into_channel_metadata_table_advanced(records)
+        insert_data_into_seed_table(seed_records)
+
+
+def get_names_of_seed_lists() -> list[dict]:
+    return fetch_seed_list_names()
+
+
+def get_seed_list_preview(my_seed_list_names: list[str]) -> list[dict]:
+    return fetch_seed_list_preview(my_seed_list_names)
+
+
+def get_seed_channel_metadata(seed_list_names: list[str]) -> list[dict]:
+    records = fetch_seed_metadata_full(seed_list_names)
+
+    # Remove private fields:
+    private_fields = ["api_response", "checkup_time", "data_source"]
+    for private_field in private_fields:
+        [record.pop(private_field) for record in records]
+
+    return records
+
+
+def get_birth_chart_data(
+    birth_chart_unit: str, seed_list_names: list[str]
+) -> list[dict]:
+    return fetch_birth_chart_data(seed_list_names, birth_chart_unit)
+
+
+def get_time_series_chart_data(
+    start_date: str,
+    end_date: str,
+    time_series_chart_unit: str,
+    seed_list_names: list[str],
+) -> list[dict]:
+    return fetch_time_series_chart_data(
+        seed_list_names, start_date, end_date, time_series_chart_unit
+    )
+
+
+def get_top_messages(
+    start_date: str,
+    end_date: str,
+    seed_list_names: list[str],
+    the_limit: int = 1000,
+):
+    records = fetch_top_messages(seed_list_names, start_date, end_date, the_limit)
+
+    # Remove api_response field
+    [record.pop("api_response") for record in records]
+
+    return records
+
+
+def generate_markdown_hyperlink(record):
+    url = f"https://t.me/{record['channel_name']}/{record['message_id']}"
+    return f"[{url}]({url})"
+
+
+def make_message_table(records: list[dict], seed_list_names: list[str]) -> list[dict]:
+    if len(records) == 0:
+        return records
+
+    df = pd.DataFrame.from_records(records)
+    df["channel_id"] = df["channel_id"].astype("int")
+
+    seed_df = pd.DataFrame.from_records(get_seed_list_preview(seed_list_names))
+    seed_df["channel_id"] = seed_df["channel_id"].astype("int")
+
+    df = df.merge(seed_df, on="channel_id", how="left")
+
+    df["url"] = df.apply(lambda x: generate_markdown_hyperlink(x), axis=1)
+
+    df = df[
+        [
+            "url",
+            "message_datetime",
+            "message_views",
+            "message_forwards",
+            "message_text",
+            "channel_name",
+            "channel_id",
+            "message_id",
+        ]
+    ]
+
+    return df.to_dict("records")
+
+
+def render_message_table(
+    start_date: str, end_date: str, seed_list_names: list[str]
+) -> list[dict]:
+    records = get_top_messages(start_date, end_date, seed_list_names)
+
+    # Create table
+    records = make_message_table(records, seed_list_names)
+    return records
+
+
+def translate_messages(records: list[dict]) -> list[dict]:
+    df = pd.DataFrame.from_records(records)
+
+    df["message_translated"] = df["message_text"].apply(
+        lambda my_text: GoogleTranslator(source="auto", target="en").translate(my_text)
+        if my_text is not None
+        else None
+    )
+
+    df = df[
+        [
+            "url",
+            "message_datetime",
+            "message_views",
+            "message_forwards",
+            "message_translated",
+            "message_text",
+            "channel_name",
+            "channel_id",
+            "message_id",
+        ]
+    ]
+
+    return df.to_dict("records")
+
+
+def send_handles_to_tg_channel_metadata_lookup_queue(data_json: dict):
+    send_data_to_queue([data_json], handles_queue)
+
+
+def send_handles_to_tg_channel_messages_lookup_queue(records: list[dict]):
+    send_data_to_queue(records, messages_queue)
+
+
+def store_channel_messages(records: list[dict]):
+    insert_data_into_channel_messages_table_advanced(records)
+
+
+def extract_data_from_message_object(message: TelegramMessage) -> dict:
+    message_dict = {
+        "channel_id": message.to_dict()["peer_id"]["channel_id"],
+        "message_id": message.to_dict()["id"],
+        "message_datetime": message.to_dict()["date"],
+        "message_views": message.to_dict()["views"],
+        "message_forwards": message.to_dict()["forwards"],
+        "message_text": message.to_dict()["message"],
+        "forwardee_channel_id": None,
+        "forwardee_message_id": None,
+        "message_is_forward": False,
+        "api_response": message.to_json(),
+    }
+
+    if message.fwd_from is not None:
+        message_dict["message_is_forward"] = True
+        if message.to_dict()["fwd_from"]["from_id"] is not None:
+            if "channel_id" in message.to_dict()["fwd_from"]["from_id"].keys():
+                message_dict["forwardee_channel_id"] = message.to_dict()["fwd_from"][
+                    "from_id"
+                ]["channel_id"]
+                message_dict["forwardee_message_id"] = message.to_dict()["fwd_from"][
+                    "channel_post"
+                ]
+
+    return message_dict
+
+
+def retrieve_channel_messages_from_telegram(
+    channel_name: str, app_name: str, api_id: int, api_hash: str
+) -> list[dict]:
+    new_max_id = 0
+    messages = []
+    with TelegramClient(app_name, api_id, api_hash) as client:
+        while True:
+            message_iterator = client.iter_messages(
+                channel_name, min_id=0, max_id=new_max_id, limit=100
+            )
+
+            try:
+                new_messages = list(message_iterator)
+            except Exception as e:
+                print(e)
+                new_messages = []
+
+            # Calculate new_max_id
+            if len(new_messages) > 0:
+                new_max_id = min([message.id for message in new_messages])
+                messages += new_messages
+
+                print(
+                    f"found {len(messages)} messages so far, and set new_max_id={new_max_id}"
+                )
+
+            # Stopping condition
+            if len(new_messages) < 100:
+                break
+
+            # Respectful pause so as not to flood the API
+            time.sleep(1)
+
+    records = [
+        extract_data_from_message_object(message)
+        for message in messages
+        if message.to_dict()["_"] == "Message"
+    ]
+
+    return records
+
+
+def retrieve_and_save_channel_messages(
+    channel_name: str, app_name: str, api_id: int, api_hash: str
+):
+    # Retrieve messages from Telegram API
+    records = retrieve_channel_messages_from_telegram(
+        channel_name, app_name, api_id, api_hash
+    )
+
+    # Save data locally
+    store_channel_messages(records)
